8a54b6b987cb754a70873cdcd955fb851ca383ff
phamphammmmm <phamtiendat246@gmail.com>
2024-02-26 00:26:14 +0700

    feat: Set up connection to ElasticSearch and Kibana containers and retrieve relevant information

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticSearch.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                throw new Exception("Error in an area ?o ");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something is not avaiable", 5);
+            }
+
+            _logger.LogInformation("Hello from action");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
